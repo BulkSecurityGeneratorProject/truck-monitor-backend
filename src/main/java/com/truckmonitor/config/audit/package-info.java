ca8d594d8b4277be7c952435a15255e7aad86b11
Iuri Pereira <iuri.pereira@talkdesk.com>
2019-07-17 09:11:20 +0100

    Initial application generated by JHipster-6.1.2

---
+++ src/main/java/com/truckmonitor/config/audit/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Audit specific code.
+ */
+package com.truckmonitor.config.audit;
---
+++ src/main/java/com/truckmonitor/repository/AuthorityRepository.java
@@ -0,0 +1,11 @@
+package com.truckmonitor.repository;
+
+import com.truckmonitor.domain.Authority;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the {@link Authority} entity.
+ */
+public interface AuthorityRepository extends JpaRepository<Authority, String> {
+}
